2747bd15066c859230a4e89b8b91837ed78a188d
saravanan <saravanan.m@quikr.com>
2019-12-11 18:19:46 +0530

    React Mounting (constructor, render) Methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bus: "BMW"
+    };
+  }
   render() {
     return <h1>Hello React!</h1>;
   }
