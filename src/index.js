566181db085690807b2733cd8129e00234bae502
saravanan <saravanan.m@quikr.com>
2019-12-11 18:14:15 +0530

    Init React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Test");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
